5968f32576473e6a0650776216d8f70dd687eb23
tatangjuliyanto <62708530+tatangjuliyanto@users.noreply.github.com>
2020-07-12 10:13:49 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,14 +1,18 @@
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="form.html">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<center>
 	<h1 width="180" align="cols">Data Anda sudah kami rekam</h1>
 	<br>
+	<div class="data">
 	<td><a href="biodata.text">Lihat validasi data anda</a><td><br>
-	<td><a href="form.html">Halaman Awal</a></td>
+	<td><a href="tabel.php"> lihat tabel pendaftaran</a><br>
+	<td><a href="form.html">Halaman Awal</a></td><br>
     </center>
-<div>
+<div class="proses">
 	
 
 
@@ -16,13 +20,12 @@
 echo "<head><title><>";
 $fp=fopen("biodata.text", "a+");
 $nama =$_POST['nama'];
-$alamat =$_POST['alamat'];
-$telp =$_POST['telp'];
+$id = $_POST['id'];
 $kelamin =$_POST['kelamin'];
-$agama =$_POST['agama'];
-$hobi =$_POST['hobi'];
+$role =$_POST['role'];
+$no =$_POST['no'];
 $prestasi =$_POST['prestasi'];
-fputs($fp,"$nama|$alamat|$telp|$kelamin|$agama|$hobi|$prestasi|\n");
+fputs($fp,"$nama|$id|$kelamin|$role|$no|$prestasi|\n");
 fclose($fp);
 ?>
 
